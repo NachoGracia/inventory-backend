9b5fb95e3158bef51c8215e2b67a7c4a54e3d092
NachoGracia <gracianacho@gmail.com>
2024-11-23 13:55:14 +0100

    itemsRoutes created

---
+++ src/routes/itemsRoutes.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const { getItems, createItem } = require("../controllers/ItemController");
+const { validateRequest } = require("../middleware/validateRequest");
+const verifyToken = require("../middleware/verifyToken ");
+
+const router = express.Router();
+
+router.get("/items", getItems);
+router.post("/items/create", validateRequest, createItem);
+
+module.exports = router;
