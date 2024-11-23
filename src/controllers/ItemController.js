edb32b9108582df943bad6e39dea75742906539f
NachoGracia <gracianacho@gmail.com>
2024-11-23 13:52:57 +0100

    itemController created

---
+++ src/controllers/ItemController.js
@@ -0,0 +1,33 @@
+const { getAllItems, insertItem } = require("../models/ItemMode");
+const capitalize = require("../utils/capitalizeString");
+
+const getItems = async (req, res) => {
+  try {
+    const items = await getAllItems();
+
+    res.status(200).json(items);
+  } catch (error) {
+    res.status(400).json({ error: "Error loading items" });
+  }
+};
+
+const createItem = async (req, res) => {
+  let { item, description = item, quantity } = req.body;
+
+  if (!item || !quantity) {
+    return res
+      .status(400)
+      .json({ error: "Item, description and quantity are required" });
+  }
+
+  const [itemCap, descriptionCap] = [item, description].map(capitalize);
+
+  try {
+    const newItem = await insertItem(itemCap, descriptionCap, quantity);
+    res.status(201).json(newItem);
+  } catch (error) {
+    res.status(400).json({ error: "Error create new item", error });
+  }
+};
+
+module.exports = { getItems, createItem };
